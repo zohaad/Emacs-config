e832f0454120c962d4214355b3412cfa5215dd7b
Zohaad Fazal <10496606+zohaad@users.noreply.github.com>
2021-07-26 01:55:58 +0200

    Full disk access + updated init.el

--- README.md
+++ README.md
@@ -8,6 +8,12 @@ Clone this repository and:
 ## Key mapping
 Place [emacs.json](./emacs.json) in `~/.config/karabiner/assets/complex_modifications/`. Open [Karabiner-Elements](https://karabiner-elements.pqrs.org/) and in the Complex modifications tab click on "Add rule", then click on "Enable All" at the top. This switches the left ctrl and ⌘ keys and assigns left ⌥/alt to the right ⌘ key. [[1]](#1)
 
+## macOS quirks
+In `System Preferences → Security & Privacy → Privacy` choose `Full Disk Access` in the left column, then add `/usr/bin/ruby`.[[2]](#2) (Press ⌘-Shift-dot to show hidden files)
+
 ## References
 <a id="1">[1]</a>
 [https://nickdrozd.github.io/2019/12/28/emacs-mac-mods.html](https://nickdrozd.github.io/2019/12/28/emacs-mac-mods.html)
+
+<a id="2">[2]</a>
+[https://emacs.stackexchange.com/a/53037](https://emacs.stackexchange.com/a/53037)
--- init.el
+++ init.el
@@ -15,18 +15,51 @@
 
 (setq custom-file (make-temp-file ""))
 
-(use-package ayu-theme
-  :config (load-theme 'ayu-light t))
+; autosaves
+(defvar emacs-autosave-directory
+  (concat user-emacs-directory "autosaves/")
+  "This variable dictates where to put auto saves. It is set to a
+directory called autosaves located wherever your .emacs.d/ is
+located.")
+(unless (file-exists-p emacs-autosave-directory)
+  (make-directory emacs-autosave-directory))
+(setq auto-save-file-name-transforms `((".*"
+					,emacs-autosave-directory t)))
+(setq auto-save-timeout 20 ; set save interval
+      auto-save-interval 20)
+(setq auto-save-default t) ; autosave every buffer that visits a file
+(setq create-lockfiles nil) ; disable lockfiles
 
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
 
+(use-package ayu-theme
+  :config (load-theme 'ayu-light t))
+
+; different fonts for text and code
 (setq @code-font "Menlo")
-(setq @text-font "Cardo")
+(setq @text-font "Fira Code")
 (setq @font-height 160)
 
 (set-face-attribute 'default nil :family @code-font :height @font-height)
 (defun @text-mode-setup ()
   (face-remap-add-relative 'default nil :family @text-font :height @font-height))
 (add-hook 'text-mode-hook '@text-mode-setup)
+
+; org-mode keybinds
+(global-set-key (kbd "C-c l") 'org-store-link)
+(global-set-key (kbd "C-c a") 'org-agenda)
+(global-set-key (kbd "C-c c") 'org-capture)
+
+; org-mode indentation
+(add-hook 'org-mode-hook 'visual-line-mode)
+(add-hook 'org-mode-hook 'org-indent-mode)
+
+; auto-complete
+(use-package company
+  :init (add-hook 'after-init-hook 'global-company-mode))
+
+; macOS specific
+(use-package exec-path-from-shell
+  :init (exec-path-from-shell-initialize))
